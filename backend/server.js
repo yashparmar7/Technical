5c85d7102bb2b6d0ab4d5fbed0ff2492a3310455
yashparmar7 <yashparmar2772004@gmail.com>
2025-09-01 14:15:01 +0530

    deploy changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import recordRouter from "./routes/records.js";
+import path from "path";
 
 import { connectDB } from "./lib/db_connect.js";
 
@@ -11,13 +12,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/record", recordRouter);
 
 const { PORT } = process.env;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE == "development"
+    ? "http://localhost:5000/record"
+    : "/record";
+
 const recordBaseUrl = axios.create({
-  baseURL: "http://localhost:5000/record",
+  baseURL: BASE_URL,
 });
 
 export default recordBaseUrl;
